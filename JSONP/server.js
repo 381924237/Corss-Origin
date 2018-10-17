7b199432344a5ad95461e0f3332952dd45e3528a
XXXXzy <381924237@qq.com>
2018-10-17 21:28:04 +0800

    addfile

---
+++ JSONP/server.js
@@ -0,0 +1,40 @@
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var url = require('url')
+
+var server = http.createServer(function(req,res) {
+  routePath(req,res)
+})
+
+function routePath(req,res) {
+  var pathObj = url.parse(req.url,true)
+  switch(pathObj.pathname) {
+    case '/getData':
+    var data = [
+      '数据1',
+      '数据2',
+      '数据3'
+    ]
+
+    if(pathObj.query.callback) {
+      res.end(pathObj.query.callback + '(' + JSON.stringify(data) + ')')
+    }else{
+      res.end(JSON.stringify(data))
+    }
+
+    break;
+
+    default:
+      fs.readFile(path.join(__dirname, pathObj.pathname), function(e, data){
+        if(e){
+          res.writeHead(404, 'not found')
+          res.end('<h1>404 Not Found</h1>')
+        }else{
+          res.end(data)
+        }
+      }) 
+  }
+}
+
+server.listen(8080)
